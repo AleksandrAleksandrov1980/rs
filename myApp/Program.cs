a0cdc403b1d05c10006c4dd9e1534bba29cb9ee1
AleksandrAleksandrov1980 <ustas_soft@mail.ru>
2022-12-01 12:05:52 +0500

    add nuget packeages

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Serilog;
+
+using var log = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
 Console.WriteLine("Hello, World!");
+
 
 int i;
 for(i=0;i<100;i++)
@@ -7,5 +15,8 @@ for(i=0;i<100;i++)
     Console.WriteLine($"Hello, World!{i}");
     int iIwwe;
 
+    log.Information($"Hello, Serilog!{i}");
+
 }
+Console.WriteLine($"VSSS!{i}");
 Console.WriteLine($"VSSS!{i}");
