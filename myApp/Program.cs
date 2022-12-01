0263f4892f31d4ddf1b2a3576cf1d57824a787a2
AleksandrAleksandrov1980 <ustas_soft@mail.ru>
2022-12-01 05:49:01 +0500

    COOM

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -1,11 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 int i;
 for(i=0;i<100;i++)
 {
     Console.WriteLine($"Hello, World!{i}");
-    int i;
+    int iI;
 
 }
 Console.WriteLine($"VSSS!{i}");
