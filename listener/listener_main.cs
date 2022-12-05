6456af3e7d2911b7fab686a518b6a1c0a85db944
AleksandrAleksandrov1980 <ustas_soft@mail.ru>
2022-12-05 18:01:47 +0500

    listener work at first time

---
+++ listener/listener_main.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Receiver about :- Hello, World!");
+
+Rabbit.CRecLog recLog = new Rabbit.CRecLog();
+recLog.Test();
