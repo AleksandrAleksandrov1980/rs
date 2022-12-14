7a903b7cabdc5981777dd437015511449c039666
AleksandrAleksandrov1980 <ustas_soft@mail.ru>
2022-12-14 16:38:06 +0500

    win service worked

--- srv_lin/Program.cs
+++ srv_lin/Program.cs
@@ -1,6 +1,10 @@
 using srv_lin;
 
 IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService(options =>
+    {
+        options.ServiceName = "SRV_LIN";
+    })
     .UseSystemd()
     .ConfigureServices(services =>
     {
--- srv_lin/Worker.cs
+++ srv_lin/Worker.cs
@@ -11,12 +11,24 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        try
         {
-            _logger.LogInformation($"Information-> running at: {DateTimeOffset.Now}" );
-            _logger.LogWarning($"Warning-> running at: {DateTimeOffset.Now}" );
-            _logger.LogError($"Error-> running at: {DateTimeOffset.Now}" );
-            await Task.Delay(1000, stoppingToken);
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation($"Information-> running at: {DateTimeOffset.Now}" );
+                _logger.LogWarning($"Warning-> running at: {DateTimeOffset.Now}" );
+                _logger.LogError($"Error-> running at: {DateTimeOffset.Now}" );
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+        catch( Exception ex)
+        {
+            _logger.LogError($"catched exeption: {ex.Message}");
+
+            //https://learn.microsoft.com/en-us/dotnet/core/extensions/windows-service 
+            // In order for the Windows Service Management system to leverage configured
+            // recovery options, we need to terminate the process with a non-zero exit code.
+            //Environment.Exit(1); 
         }
     }
 }
