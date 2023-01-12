530da9262e429326fcbdbb34178019cca27823dd
AleksandrAleksandrov1980 <ustas_soft@mail.ru>
2023-01-12 12:25:11 +0500

    mmm

--- srv_lin/notes/win_srv.bat
+++ srv_lin/notes/win_srv.bat
@@ -1,4 +1,4 @@
 sc.exe stop srv_lin
-dotnet publish "srv_lin.csproj" -c Release -o C:\projects\git_main\rs\srv_lin\publish -r win-x64 --self-contained -p:PublishTrimmed=true
+dotnet publish "C:\projects\git_main\rs\srv_lin\srv_lin.csproj" -c Release -o C:\projects\git_main\rs\srv_lin\publish -r win-x64 --self-contained -p:PublishTrimmed=true
 sc.exe start srv_lin
 sc.exe queryex srv_lin
