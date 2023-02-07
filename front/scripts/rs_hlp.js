d8bff9ef3c394ade65b46fed1d653a714161559d
AleksandrAleksandrov1980 <ustas_soft@mail.ru>
2023-02-07 12:37:16 +0500

    1

--- front/scripts/rs_hlp.js
+++ front/scripts/rs_hlp.js
@@ -14,7 +14,8 @@
 
       document.addEventListener('DOMContentLoaded', function() {
         let el = document.getElementById("path_to_dir_mk");
-        el.value = "/CALCS/"+GetTmStamp();
+        //el.value = "/CALCS/"+GetTmStamp(); //!!!! timeStamp
+        el.value = "/CALCS/2023_2_4___7_18_5_997"; 
         document.getElementById("path_to_dir_upload").value = el.value;
         //document.getElementById("path_to_file_download").value = el.value + "/results.xml";
         document.getElementById("path_to_file_download").value = el.value + "/res";
