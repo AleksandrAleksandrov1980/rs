e586e320e9ee35338ef059d99b122b62fb7df505
AleksandrAleksandrov1980 <ustas_soft@mail.ru>
2023-04-26 13:51:45 +0500

    blazor

--- wmvc/Program.cs
+++ wmvc/Program.cs
@@ -1,25 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if(!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+else
+{
+    app.UseDeveloperExceptionPage(); // ustas enlarge exception description
+}
+app.UseStatusCodePages(); //errs1
+app.UseStatusCodePagesWithReExecute("/{0}"); //errs2
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
