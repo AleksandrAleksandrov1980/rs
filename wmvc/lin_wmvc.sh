b4f9e0e35fb58cdf0e8ce819a81748749c08449b
AleksandrAleksandrov1980 <ustas_soft@mail.ru>
2023-06-16 13:32:10 +0500

    for nix script

---
+++ wmvc/lin_wmvc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#sudo systemctl stop srv_lin.service # stop service to release any file locks which could conflict with dotnet publish
+dotnet publish /home/ustas/projects/git_main/rs/wmvc/wmvc.csproj -c Release -o /home/ustas/projects/git_main/rs/wmvc/wmvc -r linux-x64 --self-contained -p:PublishTrimmed=true
+#sudo cp /home/ustas/projects/git_main/rs/srv_lin/srv_lin.service /etc/systemd/system/srv_lin.service
+#sudo systemctl daemon-reload
+#systemctl start srv_lin.service  
+#sudo systemctl status srv_lin.service
+
