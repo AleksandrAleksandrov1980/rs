d892e52719518dcf2f9d305d9b82f3aee826782f
AleksandrAleksandrov1980 <ustas_soft@mail.ru>
2023-09-27 17:31:11 +0500

    second

--- send_to_web/Program.cs
+++ send_to_web/Program.cs
@@ -3,6 +3,8 @@ using RabbitMQ.Client;
 using Serilog;
 using System.Text;
 using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using static System.Net.WebRequestMethods;
 
 namespace send_to_web;
 
@@ -17,6 +19,16 @@ class Program
     static void Main(string[] args) {
         try{
             string str_path_exe_dir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            IConfiguration config = new ConfigurationBuilder()
+                    .SetBasePath(str_path_exe_dir)
+                    .AddJsonFile("appsettings.json")
+                    .Build();                    
+            IConfigurationSection con_sec_params = config.GetSection("r_params");
+            string str_q_host = con_sec_params.GetRequiredSection("q_host").Value ?? "";
+            int    n_q_port   = int.Parse(con_sec_params.GetRequiredSection("q_port").Value ?? "5672");
+            string str_q_user = con_sec_params.GetRequiredSection("q_user").Value ?? "";
+            string str_q_pass = con_sec_params.GetRequiredSection("q_pass").Value ?? "";
+            string str_q_exch = con_sec_params.GetRequiredSection("q_exch").Value ?? "";
             string str_exe_file_name = System.AppDomain.CurrentDomain.FriendlyName;
             string str_path_log = str_path_exe_dir+Path.DirectorySeparatorChar+System.IO.Path.GetFileNameWithoutExtension( str_exe_file_name )+".log";
             Console.WriteLine($"log:{str_path_log}"); 
@@ -27,6 +39,10 @@ class Program
                     rollingInterval: RollingInterval.Day,
                     rollOnFileSizeLimit: true)
                 .CreateLogger();
+            Log.Information($"q_host:{str_q_host}");
+            Log.Information($"q_port:{n_q_port}");
+            Log.Information($"q_user:{str_q_user}");
+            Log.Information($"q_user:{str_q_exch}");
             Rastr rastr = new ASTRALib.Rastr();
             Log.Information("Create Rastr");
             string str_path_to_after_oc_file = args[0];
@@ -44,23 +60,21 @@ class Program
                 lstHlps.Add( hlp );
             }
             string str_json_hlps = JsonSerializer.Serialize(lstHlps);
-            const string str_exch_name = "tele";
             ConnectionFactory factory = new ConnectionFactory();
-
-            factory.HostName    = "192.168.1.59";
-            factory.Port        = 5672;
+            factory.HostName    = str_q_host;
+            factory.Port        = n_q_port;
             factory.VirtualHost = "/";
-            factory.UserName    = "rastr"; // guest - resctricted to local only
-            factory.Password    = "rastr";
+            factory.UserName    = str_q_user; // guest - resctricted to local only
+            factory.Password    = str_q_pass;
             Log.Warning($"CONNECTING {factory.HostName}:{factory.Port} = {factory.UserName}");
             using(IConnection connection = factory.CreateConnection()){ 
                 using(IModel exchange = connection.CreateModel()){ 
-                    Log.Warning($"EXCHANGE_ declare-> [{str_exch_name}]");
-                    exchange.ExchangeDeclare( exchange: str_exch_name, type: ExchangeType.Fanout, durable: false, autoDelete:false );
-                    Log.Information($"EXCHANGE_ declared-> [{str_exch_name}]");
+                    Log.Warning($"EXCHANGE_ declare-> [{str_q_exch}]");
+                    exchange.ExchangeDeclare( exchange: str_q_exch, type: ExchangeType.Fanout, durable: false, autoDelete:false );
+                    Log.Information($"EXCHANGE_ declared-> [{str_q_exch}]");
                     byte[] body = Encoding.UTF8.GetBytes(str_json_hlps);
-                    exchange.BasicPublish( exchange: str_exch_name, routingKey: "", basicProperties: null, body: body );
-                    Log.Information($"to: {str_exch_name} : count : {lstHlps.Count}");
+                    exchange.BasicPublish( exchange: str_q_exch, routingKey: "", basicProperties: null, body: body );
+                    Log.Information($"send to: {str_q_exch} : count : {lstHlps.Count}");
                 }
             }
             Log.Information("That's all folks!");
