df3f9c8392f36b22b10be12795f42058b60ecc13
AleksandrAleksandrov1980 <ustas_soft@mail.ru>
2023-09-28 11:52:44 +0500

    first worked HTML

--- front/scripts/tele_hlp.js
+++ front/scripts/tele_hlp.js
@@ -3,36 +3,31 @@
 
 class CStompTele
 {
-
+  
   constructor(){
     this.name_exchng         = "rastr_tele";
- }
+  }
 
- static log(str_log){
-   console.log( '['+this.name_exchng + ']: ' + str_log);
- }
-
+  log(str_log){
+     console.log( '['+this.name_exchng + ']: ' + str_log);
+  }
 
   callback_subscribe (message){
     try{
       if (message.body){
-        CStompHlp.log('got msg with body : ' + message.body);
+        this.log('got msg with body : ' + message.body);
         this._nCounterMsgs++;
         //this.AddLog( "< " + this._nCounterMsgs.toString()+ " : " + message.body );
         let obj_msg = JSON.parse(message.body);
         obj_msg.num = this._nCounterMsgs;
-        this._arr_objs.push(obj_msg);
-       } else {
-          CStompHlp.log('got empty message'); 
+      } else {
+          this.log('got empty message'); 
       }
     }catch(err){
-      CStompHlp.log('catched exception!: ' + err);
+      this.log('catched exception!: ' + err);
       this.AddLog( "< " + this._nCounterMsgs.toString()+ "  error parse! : " + message.body );
-   }
+    }
   };
-
-  AddLog(str_log) {
-  }
 
   connect(){
     //brokerURL : 'ws://192.168.1.59:15674/ws',
@@ -43,29 +38,27 @@ class CStompTele
         login    : 'rastr',
         passcode : 'rastr',
       },
-      reconnectDelay:    1000,
-      heartbeatIncoming: 4000,
-      heartbeatOutgoing: 4000,
+      reconnectDelay:    5000,
+      heartbeatIncoming: 10000,
+      heartbeatOutgoing: 10000,
       debug: function (str){
-        //CStompHlp.log(str);
           console.log('debug: '+str);
       },
     });
 
+    this.log('connected to: '+this.client.brokerURL);
+
     this.client.onConnect = function (frame) {
       // Do something, all subscribes must be done is this callback
       // This is needed because this will be executed after a (re)connect
-      CStompHlp.log('onConnect: trying subscribe to ' + this.name_exchng + ' <> ' + this.callback_subscribe );
+      this.log('onConnect: trying subscribe to ' + this.name_exchng + ' <> ' + this.callback_subscribe );
       //when subscription closed, websocket already closed!!! and reopend in cycle!!
       this.subscription_events = this.client.subscribe( this.name_exchng, this.callback_subscribe.bind(this) );
-      this.table_populate = this.table_populate.bind(this);
-      this.SetChnlState(true);
     };
     this.client.onConnect = this.client.onConnect.bind(this);
 
     this.client.onDisconnect = (frame) => {
-      CStompHlp.log('onDisconnect : ' + frame.body);
-      this.SetChnlState(false);
+      this.log('onDisconnect : ' + frame.body);
     }
     this.client.onDisconnect = this.client.onDisconnect.bind(this);
 
@@ -74,26 +67,25 @@ class CStompTele
       // Bad login/passcode typically will cause an error
       // Complaint brokers will set `message` header with a brief message. Body may contain details.
       // Compliant brokers will terminate the connection after any error
-      CStompHlp.log('onStompError: ' + frame.headers['message']);
-      CStompHlp.log('onStompError Additional details: ' + frame.body);
+      this.log('onStompError: ' + frame.headers['message']);
+      this.log('onStompError Additional details: ' + frame.body);
     };
 
     this.client.onWebSocketClose = function (evt)  {
-      CStompHlp.log( 'onWebSocketClose: ' + evt );
-      this.SetChnlState(false);
+      this.log( 'onWebSocketClose: ' + evt );
     };
     this.client.onWebSocketClose = this.client.onWebSocketClose.bind(this)
 
     this.client.onUnhandledMessage = function (message) {
-      CStompHlp.log( 'onUnhandledMessage: message = ' + message );
+      this.log( 'onUnhandledMessage: message = ' + message );
     };
 
     this.client.onUnhandledReceipt = function (frame)  {
-      CStompHlp.log('onUnhandledReceipt: ' + frame.body);
+      this.log('onUnhandledReceipt: ' + frame.body);
     };
 
     this.client.onUnhandledFrame  = function (frame)  {
-      CStompHlp.log('onUnhandledFrame: ' + frame.body);
+      this.log('onUnhandledFrame: ' + frame.body);
     };
 
     this.client.activate();
@@ -104,22 +96,5 @@ class CStompTele
 var g_StompTele   = new CStompTele();
 g_StompTele.connect();
 
-
-/*
-let client = new StompJs.Client({
-              brokerURL: 'ws://alexandrov-7k:15674/ws',
-              connectHeaders: {
-                login    : 'rastr',
-                passcode : 'rastr',
-              },
-              reconnectDelay:    1000,
-              heartbeatIncoming: 4000,
-              heartbeatOutgoing: 4000,
-              debug: function (str){
-                CStompHlp.log(str);
-              },
-            });
-
-*/
-    
-alert("fwefe");
+   
+//alert("fwefe");
