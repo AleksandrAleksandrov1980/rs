884f04930de2e30c9944976e45f40c2815a7aa73
AleksandrAleksandrov1980 <ustas_soft@mail.ru>
2023-11-13 15:22:22 +0500

    upload db

--- wmvc/pg_write_dump.bat
+++ wmvc/pg_write_dump.bat
@@ -2,3 +2,8 @@ rem pg_dump -h [host address] -Fc -o -U [database user] <database name> > [dump 
 rem Host=192.168.1.84;Database=rastrwin;Username=postgres;Password=pgadmin
 rem SET PGPASSWORD="pgadmin"
 "C:\Program Files\PostgreSQL\16\bin\pg_dump" -h 192.168.1.84 -Fc  -U postgres rastrwin > pg_dump.bin
+rem SET new_db="rastrwin"
+rem SET db_dump_file="C:\projects\git_main\rs\wmvc\pg_dump.bin"
+rem "C:\Program Files\PostgreSQL\16\bin\dropdb"   -h msk-N9E-PSU5.ntc.ntcees.ru -p 5432 -U postgres --if-exists  %new_db%
+rem "C:\Program Files\PostgreSQL\16\bin\createdb" -h msk-N9E-PSU5.ntc.ntcees.ru -p 5432 -U postgres -T template0 %new_db%
+rem "C:\Program Files\PostgreSQL\16\bin\pg_restore" -h msk-N9E-PSU5.ntc.ntcees.ru -p 5432 -U postgres -d %new_db% %db_dump_file%
